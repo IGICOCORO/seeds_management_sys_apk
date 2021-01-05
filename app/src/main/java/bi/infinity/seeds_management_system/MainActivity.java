85bcfe813538c8843af68486c4a684564a04c9fc
GBstallman <nduwimanaguymich@gmail.com>
2021-01-05 13:22:26 -0800

    initial commit

---
+++ app/src/main/java/bi/infinity/seeds_management_system/MainActivity.java
@@ -0,0 +1,18 @@
+package bi.infinity.seeds_management_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void performLogin(View view) {
+    }
+}
