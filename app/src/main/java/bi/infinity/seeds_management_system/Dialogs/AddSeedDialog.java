aa89af3e6b04dbb38101bfd918748081b7fac69f
GBstallman <nduwimanaguymich@gmail.com>
2021-02-19 09:00:25 +0200

    resolved display add_seed_dialog

---
+++ app/src/main/java/bi/infinity/seeds_management_system/Dialogs/AddSeedDialog.java
@@ -0,0 +1,37 @@
+package bi.infinity.seeds_management_system.Dialogs;
+
+import android.app.Dialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import bi.infinity.seeds_management_system.Model.Seed;
+import bi.infinity.seeds_management_system.R;
+
+import static bi.infinity.seeds_management_system.R.style.Theme_AppCompat_DayNight_Dialog;
+
+
+public class AddSeedDialog extends Dialog {
+    Seed seed;
+    Context context;
+    EditText field_seed_nom, field_seed_date, field_seed_company_from, field_seed_prix,field_seed_qtt;
+    Button btn_add_seed;
+
+    public AddSeedDialog(Context context) {
+        super(context, Theme_AppCompat_DayNight_Dialog);
+        setContentView(R.layout.add_seed_dialog);
+        this.context = context;
+
+
+        field_seed_nom = findViewById(R.id.field_seed_nom);
+        field_seed_prix = findViewById(R.id.field_seed_prix);
+        field_seed_qtt = findViewById(R.id.field_seed_qtt);
+        field_seed_company_from = findViewById(R.id.field_seed_company_from);
+        field_seed_date = findViewById(R.id.field_seed_date);
+        btn_add_seed = findViewById(R.id.btn_seed_cancel);
+        btn_add_seed = findViewById(R.id.btn_seed_submit);
+    }
+
+}
--- app/src/main/java/bi/infinity/seeds_management_system/Fragments/SeedsFragment.java
+++ app/src/main/java/bi/infinity/seeds_management_system/Fragments/SeedsFragment.java
@@ -3,6 +3,7 @@ package bi.infinity.seeds_management_system.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -10,9 +11,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 
 import bi.infinity.seeds_management_system.Adapters.AdapterStockSeed;
+import bi.infinity.seeds_management_system.Dialogs.AddSeedDialog;
 import bi.infinity.seeds_management_system.Model.Seed;
 import bi.infinity.seeds_management_system.Model.Stock;
 import bi.infinity.seeds_management_system.R;
@@ -20,26 +24,52 @@ import bi.infinity.seeds_management_system.R;
 
 public class SeedsFragment extends Fragment {
     private RecyclerView recycler_seeds;
-    private ArrayList<Stock> stocks;
+    private ArrayList<Seed> seeds;
     private AdapterStockSeed adapter;
+    private FloatingActionButton fb_seed_add;
+    private ArrayList<Stock> stocks;
+
+    public SeedsFragment(RecyclerView recycler_seeds, ArrayList<Stock> stocks, FloatingActionButton fb_emission_add, AdapterStockSeed adapter) {
+        this.recycler_seeds = recycler_seeds;
+        this.stocks = stocks;
+        this.fb_seed_add = fb_seed_add;
+        this.adapter = adapter;
+    }
+
+    public SeedsFragment() {
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_seeds, container, false);
 
+        fb_seed_add = view.findViewById(R.id.fb_seed_add);
+
         recycler_seeds = view.findViewById(R.id.recycler_seeds_item);
         recycler_seeds.setLayoutManager(new GridLayoutManager(getContext(),1));
+        recycler_seeds.addItemDecoration(
+                new DividerItemDecoration(recycler_seeds.getContext(), DividerItemDecoration.VERTICAL)
+        );
 
         stocks = new ArrayList<>();
         adapter = new AdapterStockSeed(stocks, getContext());
         recycler_seeds.setAdapter(adapter);
+
+        fb_seed_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AddSeedDialog(getContext()).show();
+            }
+        });
         getStock();
         return  view;
     }
 
     private void getStock() {
-        Stock stock = new Stock("Riz","sans","Details du semence", "Nitrains", "2000", "60");
+        Stock stock = new Stock("umuceri","sans","Details du semence", "Nitrains", "2000", "60");
         stocks.add(stock);
         stocks.add(stock);
         stocks.add(stock);
