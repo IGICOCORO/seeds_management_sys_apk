f076430a7a694666219c128a4fbb8c08ec8b5a3e
GBstallman <nduwimanaguymich@gmail.com>
2021-02-19 12:52:24 +0200

    Added appBar and menu

--- app/src/main/java/bi/infinity/seeds_management_system/DashboardActivity.java
+++ app/src/main/java/bi/infinity/seeds_management_system/DashboardActivity.java
@@ -1,17 +1,18 @@
 package bi.infinity.seeds_management_system;
 
 
-import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
-
 import com.google.android.material.tabs.TabLayout;
 
 public class DashboardActivity extends AppCompatActivity {
-    @Override
+     private Toolbar toolbar;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tablayout_menu);
@@ -19,12 +20,24 @@ public class DashboardActivity extends AppCompatActivity {
         ViewPager viewPager = findViewById(R.id.viewpager);
         PageAdapter pageAdapter = new PageAdapter(getSupportFragmentManager());
 
+        toolbar = findViewById(R.id.main_toolbar);
+        toolbar.setTitle("Seeds");
+        getSupportActionBar();
+
+
         viewPager.setAdapter(pageAdapter);
         tabHome.setupWithViewPager(viewPager);
 
         tabHome.getTabAt(0).setText("Seeds").setIcon(R.drawable.ic_home);
         tabHome.getTabAt(1).setText("Stock").setIcon(R.drawable.ic_info);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true ;
     }
 
 }
