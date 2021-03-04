a14bc7de9887ae486f73f02b247b733b3557eb32
GBstallman <nduwimanaguymich@gmail.com>
2021-03-04 10:10:06 +0200

    created VenteDialog and vendre_seed_dialog ressources

---
+++ app/src/main/java/bi/infinity/seeds_management_system/Dialogs/VenteDialog.java
@@ -0,0 +1,33 @@
+package bi.infinity.seeds_management_system.Dialogs;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.widget.Button;
+import android.widget.EditText;
+
+import bi.infinity.seeds_management_system.Model.Seed;
+import bi.infinity.seeds_management_system.R;
+
+import static bi.infinity.seeds_management_system.R.style.Theme_AppCompat_DayNight_Dialog;
+
+public class VenteDialog extends Dialog {
+    Seed seed;
+    Context context;
+    EditText field_seed_nom, field_seed_date, field_seed_company_from, field_seed_prix,field_seed_qtt;
+    Button btn_add_seed;
+
+    public VenteDialog(Context context) {
+        super(context, Theme_AppCompat_DayNight_Dialog);
+        setContentView(R.layout.vendre_seed_dialog);
+        this.context = context;
+
+
+        field_seed_nom = findViewById(R.id.field_seed_nom);
+        field_seed_prix = findViewById(R.id.field_seed_prix);
+        field_seed_qtt = findViewById(R.id.field_seed_qtt);
+        field_seed_company_from = findViewById(R.id.field_seed_company_from);
+        field_seed_date = findViewById(R.id.field_seed_date);
+        btn_add_seed = findViewById(R.id.btn_seed_cancel);
+        btn_add_seed = findViewById(R.id.btn_seed_submit);
+    }
+}
