c964f1f6d23852ac49f5a3ac89e135bb25c9b34c
GBstallman <nduwimanaguymich@gmail.com>
2021-04-14 02:40:14 -0700

    created UserChoiceActivity and activity_user_choice_activity

---
+++ app/src/main/java/bi/infinity/seeds_management_system/UserChoiceActivity.java
@@ -0,0 +1,14 @@
+package bi.infinity.seeds_management_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_choice_activity);
+    }
+}
