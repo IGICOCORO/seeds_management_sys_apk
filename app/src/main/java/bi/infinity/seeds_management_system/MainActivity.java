c4a8dd55b48306d51e9096c8b5ed03015f863c70
GBstallman <nduwimanaguymich@gmail.com>
2021-04-19 02:21:13 -0700

    created drawer_navigation and ressources

--- app/src/main/java/bi/infinity/seeds_management_system/MainActivity.java
+++ app/src/main/java/bi/infinity/seeds_management_system/MainActivity.java
@@ -1,13 +1,19 @@
 package bi.infinity.seeds_management_system;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import bi.infinity.seeds_management_system.Fragments.AddFragmentPlant;
 import bi.infinity.seeds_management_system.Fragments.FragmentStock;
 import bi.infinity.seeds_management_system.Fragments.HomeFragmementNew;
-import bi.infinity.seeds_management_system.Fragments.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +21,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-       getSupportFragmentManager().beginTransaction()
-               .replace(R.id.fragment_container, new AddFragmentPlant())
-               .addToBackStack(null)
-               .commit();
-
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, new HomeFragmementNew())
+                .commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @SuppressLint("NonConstantResourceId")
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+                    switch (menuItem.getItemId()) {
+                        case R.id.action_home:
+                            selectedFragment = new HomeFragmementNew();
+                            break;
+                        case R.id.action_stock:
+                            selectedFragment = new FragmentStock();
+                            break;
+                        case R.id.action_add:
+                            selectedFragment = new AddFragmentPlant();
+                            break;
+
+                    }
+                    if (selectedFragment != null) {
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fragment_container, selectedFragment)
+                                .commit();
+                    }
+                    return true;
+                }
+
+
+            };
 }
